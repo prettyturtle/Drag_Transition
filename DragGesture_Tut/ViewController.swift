2382ba661a82fbc444129be7637175f58de4bd1b
prettyturtle <unifconti@gmail.com>
2023-08-28 22:02:28 +0900

    Initial Commit

---
+++ DragGesture_Tut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragGesture_Tut
+//
+//  Created by yc on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
